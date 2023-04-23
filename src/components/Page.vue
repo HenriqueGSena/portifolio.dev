2949fd46eaf1beb7fb0d8038dbc0512121a56f3d
HenriqueGSena <carloshenrique3250@gmail.com>
2023-04-23 18:39:05 -0300

    Teste de projeto recem criado e criando novos componentes

---
+++ src/components/Page.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>
+        Ola Mundo
+    </h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    name: "Page"
+})
+export default class Page extends Vue { }
+</script>
+
+<style scoped>
+*{
+    background: rebeccapurple;
+}
+</style>
