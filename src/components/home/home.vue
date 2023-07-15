5cf6787ce469da5cd9737462a5fc2812acbb2fc6
HenriqueGSena <carloshenrique3250@gmail.com>
2023-07-14 21:50:39 -0300

    portifolio-dev-v0.02
    * Refatorando apresentacao com o nome na pagina e estilizando a tela;

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,8 +2,23 @@
   <div class="home-page">
     <div class="container">
       <div class="row">
-        <div class="col-6 bg-primary" style="margin-top: 40%; margin-bottom: auto">
-          <p>lorem iplkdsvbnkljsbndjkvbnklsjbnvkljnsjklvnslvnklsv</p>
+        <div class="col-6" style="margin-top: 40%; margin-bottom: auto">
+          <div class="page-title">
+            <div class="text-1">
+              <span v-text="$t('portfolioDevTranslate.homePage.apresentationName')"></span>
+            </div>
+            <div class="text-2">
+              <span v-text="$t('portfolioDevTranslate.homePage.titleName')"></span>
+            </div>
+            <div class="text-3">
+              <span v-text="$t('portfolioDevTranslate.homePage.profession')"></span>
+            </div>
+            <div>
+              <button type="button" class="btn btn-outline-success">
+                <span v-text="$t('portfolioDevTranslate.homePage.button')"></span>
+              </button>
+            </div>
+          </div>
         </div>
         <div class="col-6">
           <img src="@/assets/dev.svg" class="img-fluid" alt="DevLog">
@@ -20,11 +35,37 @@
   color: #ffffff;
   background-color: #242423;
   height: 100vh;
-  font-family: 'Poppins', sans-serif;
 }
 
 .img-fluid {
   margin-top: 7vh;
   height: 90vh;
+}
+
+.page-title {
+  font-family: 'Poppins', sans-serif;
+}
+
+.text-1 {
+  font-size: 27px;
+}
+
+.text-2 {
+  color: #FFF;
+  font-family: DM Sans;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.text-3 {
+  color: var(--gray-3, #828282);
+  font-family: DM Sans;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 550;
+  line-height: normal;
+  width: 215.043px;
 }
 </style>
