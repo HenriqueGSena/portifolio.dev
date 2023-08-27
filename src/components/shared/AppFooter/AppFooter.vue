ea52fc19151bca6f1793797aecec3657b83fc9b0
HenriqueGSena <carloshenrique3250@gmail.com>
2023-08-27 12:48:27 -0300

    portifolio-dev-v0.03
    * Finalizando o componente footer na tela

--- src/components/shared/AppFooter/AppFooter.vue
+++ src/components/shared/AppFooter/AppFooter.vue
@@ -7,29 +7,29 @@
       <div class="container-fluid">
         <section class="mb-4">
           <!-- Github -->
-          <a class="btn text-white btn-floating m-2 custom-background rounded-circle"
-              href="#!"
+          <a class="btn text-white btn-floating m-2 custom-background rounded-circle no-hover"
+              href="https://github.com/HenriqueGSena" target="_blank"
               role="button"
           ><i class="bi bi-github custom-icon"></i>
           </a>
 
           <!-- Twitter -->
-          <a class="btn text-white btn-floating m-2 custom-background rounded-circle"
-              href="#!"
+          <a class="btn text-white btn-floating m-2 custom-background rounded-circle no-hover"
+              href="https://twitter.com/Henrique_sena26" target="_blank"
               role="button"
           ><i class="bi bi-twitter custom-icon"></i>
           </a>
 
           <!-- Linkedin -->
-          <a class="btn text-white btn-floating m-2 custom-background rounded-circle"
-              href="#!"
+          <a class="btn text-white btn-floating m-2 custom-background rounded-circle no-hover"
+              href="https://www.linkedin.com/in/carloshenrique26/" target="_blank"
               role="button"
           ><i class="bi bi-linkedin custom-icon"></i>
           </a>
 
           <!-- Instagram -->
-          <a class="btn text-white btn-floating m-2 custom-background rounded-circle"
-              href="#!"
+          <a class="btn text-white btn-floating m-2 custom-background rounded-circle no-hover"
+              href="https://www.instagram.com/henrique.sena23/" target="_blank"
               role="button"
           ><i class="bi bi-instagram custom-icon"></i>
           </a>
@@ -50,5 +50,14 @@
 }
 .rounded-circle {
   border-radius: 60%;
+}
+.no-hover {
+  transition: none !important;
+}
+.no-hover:hover {
+  transition: none !important;
+  color: inherit !important;
+  background-color: #27AE60 !important;
+  border-color: #111111 !important;
 }
 </style>
