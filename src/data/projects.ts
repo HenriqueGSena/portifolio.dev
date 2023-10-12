e4c8c82fad08dea0ad6762350382827de8ce73ad
HenriqueSena <carloshenrique3250@gmail.com>
2023-10-11 21:58:13 -0300

    portifolio-dev-v0.05
    * Montando database para gerar cards;
    * Incluindo imagens do projeto;

---
+++ src/data/projects.ts
@@ -0,0 +1,10 @@
+const projects = [
+    {
+        id: 1,
+        title: 'News Express',
+        category: 'Web Application',
+        img: require('@/assets/projects/new-express.png'),
+    }
+];
+
+export default projects;
