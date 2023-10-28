37d4d1a124f0dd4c9ef7a9f824b35e364dedd856
HenriqueSena <carloshenrique3250@gmail.com>
2023-10-27 21:35:35 -0300

    portifolio-dev-v0.05
    * Alerando imagem de do card;
    * Finalizando ajustes de card para receber dados;

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -1,14 +1,27 @@
 <template>
   <section>
-    <div class="container" style="margin-top: 52px">
-      <div class="row justify-content-center">
-        <div class="col-3" v-for="project in projects" :key="project.id">
-          <div class="card">
-            <img class="card-img-top mt-lg-2" :src="project.img" alt="ImageProject">
-            <div class="card-body">
-              <h5 class="card-title text-center" style="margin: 0">{{ project.title }}</h5>
-              <p class="card-text text-center">{{ project.category }}</p>
-              <a :href="project.url" target="_blank" class="btn btn-primary">Saiba mais</a>
+    <div
+        class="container" style="margin-top: 52px"
+    >
+      <div
+          class="row justify-content-center"
+      >
+        <div
+            class="col-3" v-for="project in projects" :key="project.id"
+        >
+          <div class="card border-0 shadow">
+            <img class="img-thumbnail"
+                 :src="project.img"
+                 alt="ImageProject"
+            >
+            <div class="card-body text-light bg-dark">
+              <h5 class="card-title text-center"
+                  style="margin: 0"
+              >
+                {{ project.title }}
+              </h5>
+              <p class="card-text text-center mt-3" style="margin: 0">{{ project.category }}</p>
+              <a :href="project.url" target="_blank" class="btn btn-success mt-4">Saiba mais</a>
             </div>
           </div>
         </div>
