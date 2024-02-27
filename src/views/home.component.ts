15ea92c360e30800d4d2828181566eed4300fdb0
henrique.sena <carloshenrique3250@gmail.com>
2024-02-27 20:29:05 -0300

    port-dev-v0.06
    * Ajuste do link whatsapp;
    * Adicao de informacoes no curriculo-sena.pdf;

--- src/views/home.component.ts
+++ src/views/home.component.ts
@@ -1,5 +1,4 @@
 import { Options, Vue } from "vue-class-component";
-import { Prop } from "vue-property-decorator";
 import AppBanner from "@/components/shared/AppBanner/AppBanner.vue";
 import ProjectsGrid from "@/components/projects/ProjectsGrid.vue";
 
@@ -10,9 +9,8 @@ import ProjectsGrid from "@/components/projects/ProjectsGrid.vue";
     }
 })
 export default class home extends Vue {
-    @Prop({default: '5521992834373'}) whatsappNumber!: string;
 
     get whatsappLink():string {
-        return `https://wa.me/${this.whatsappNumber}`;
+        return `https://wa.me/5521992834373`;
     }
 }
