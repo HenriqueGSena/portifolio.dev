cbde5c78c22968bd6c57860a41a6be04e697f1fd
henrique.sena <carloshenrique3250@gmail.com>
2024-03-18 18:46:41 -0300

    port-dev-v0.07
    * Instalando pacote axios;
    * Criando service consumo de api github;
    * Chamando service dentro do ProjectsGrid.component.ts;

--- src/components/projects/ProjectsGrid.component.ts
+++ src/components/projects/ProjectsGrid.component.ts
@@ -1,7 +1,9 @@
 import {Vue} from "vue-class-component";
+import portfolioService from "@/service/portfolioService";
 import projectsData from '../../data/projects';
 
 export default class ProjectsGrid extends Vue {
 
     projects = projectsData;
+    private portfolioService!: () => portfolioService;
 }
