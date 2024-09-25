19226b7d19ab4c396579c1b3182c694cbc485fb6
henrique.sena <carloshenrique3250@gmail.com>
2024-09-25 19:59:30 -0300

    port-dev-v0.08
    * Ajuste nos cards de projeto;
    * Ajuste no menu da tela;
    * Fixando botao whatsapp na tela;

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -4,7 +4,7 @@
       <div class="row justify-content-center">
         <div class="col-md-5 col-12 mb-4 justify-content-center" v-for="(repo, index) in repositories.slice(0,6)" :key="repo.id"
           :class="{ 'mr-4': index !== repositories.length - 1 }">
-          <div class="card rounded-0 border-0 shadow">
+          <div class="card rounded-0 border-0">
             <div class="card-body text-light" style="background-color: #1B1B1B;">
               <h5 class="card-title text-center pb-3" style="margin: 0">{{ repo.name }}</h5>
               <div class="container">
